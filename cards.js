638e33a323169ef286bdc0d65dbe3315c72a97f2
Tor Kingdon <tor@hearkitty.com>
2021-07-30 20:22:04 -0600

    mostly done

--- cards.js
+++ cards.js
@@ -2,23 +2,31 @@
 // step 3
 // cards page
 
-let deck;
-let nextCard;
 const newCardButt = document.querySelector('#new-card');
+const cardTable = document.querySelector('#table');
 
-document.onload = () => {
-    deck = axios
+let deck;
+let deckPromise = axios
     .get('http://deckofcardsapi.com/api/deck/new/shuffle/')
     .then(res => {
-        return res.data.deck_id;
+        deck = res.data.deck_id;
+        return deck;
     })
     .catch(err => {
         return err;
     });
+
+newCardButt.addEventListener('click', (e) => {
+    let nextCard = axios
+        .get(`http://deckofcardsapi.com/api/deck/${deck}/draw/?count=1`)
+        .then(res => {
+            return dealCard(res.data.cards[0].image)
+        })
+})
+
+function dealCard (url) {
+    card = document.createElement('img');
+    card.src = url;
+    card.classList.add('card');
+    cardTable.appendChild(card);
 }
-
-let nextCard = axios
-    .get(`http://deckofcardsapi.com/api/deck/${deck}/draw/?count=1`)
-    .then(data => {
-
-    })
